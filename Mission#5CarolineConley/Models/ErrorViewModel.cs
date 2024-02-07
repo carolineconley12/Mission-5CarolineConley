a1dd50eb6474163e4d9d36b324f5709dff7b5578
carolineconley12 <cconley6@byu.edu>
2024-02-07 09:05:14 -0700

    Add project files.

---
+++ Mission#5CarolineConley/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission_5CarolineConley.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Mission#5CarolineConley/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    $('#calculate').click(function () {
+        var hours = parseInt($('#hours').val());
+        if (!isNaN(hours) && hours > 0) {
+            var rate = 50; // Your tutoring rate per hour
+            var total = hours * rate;
+            $('#total').text('$' + total.toFixed(2));
+        } else {
+            alert('Please enter a valid positive number of hours.');
+        }
+    });
+});
